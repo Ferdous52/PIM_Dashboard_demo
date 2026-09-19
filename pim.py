c53fb79fa57f5d0fdd391b54c00eb244d8aa9404
Sk. Ferdous Hossain <50297388+Ferdous52@users.noreply.github.com>
2026-09-19 22:53:23 +0600

    Update pim.py

--- pim.py
+++ pim.py
@@ -276,16 +276,9 @@ def dashboard_page():
         
         col1, col2, col3, col4 = st.columns(4)
         
-        st.markdown("""<style>div[data-testid="stMetric"] {background-color: #FFFFFF ;border: 1px solid #E2E8F0;padding: 20px;border-radius: 12px;box-shadow: 0 2px 8px rgba(0,0,0,0.08);}</style>""", unsafe_allow_html=True)
+        st.markdown("""<style>div[data-testid="stMetric"] {background-color: light_olive ;border: 1px solid #E2E8F0;padding: 20px;border-radius: 12px;box-shadow: 0 2px 8px rgba(0,0,0,0.08);}</style>""", unsafe_allow_html=True)
         
         with col1:
-            st.markdown("""<style>
-            .card {padding: 20px;border-radius: 15px;text-align: center;box-shadow: 0 4px 12px rgba(0,0,0,0.08);}
-            .card-blue {background-color: #DBEAFE;}
-            </style>""", unsafe_allow_html=True)
-
-            
-            st.markdown("""<div class="card card-blue"><h4>Total Schools</h4><h2>120</h2></div>""", unsafe_allow_html=True)
             total_schools = df["School Name"].nunique()
             st.metric("Total Schools", total_schools)
             
