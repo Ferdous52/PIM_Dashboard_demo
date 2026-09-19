9647b69ab4b41c066173c6baf99b2f6c570cb09c
Sk. Ferdous Hossain <50297388+Ferdous52@users.noreply.github.com>
2026-09-19 13:48:02 +0600

    Update pim.py

--- pim.py
+++ pim.py
@@ -279,7 +279,7 @@ def dashboard_page():
         df1 = df.copy()
         table_1 = pd.pivot_table(df1,index="RtR Staff Name",values=["School Name","Teacher Name"],aggfunc={"School Name":"nunique", "Teacher Name": "count"},margins=True,margins_name="Total")
         Schl_Distn = table_1.reset_index()
-        st.dataframe(Schl_Distn,use_container_width=True)
+        st.dataframe(Schl_Distn,width="content")
 
     elif page == "Schools":
         st.subheader("Schools")
